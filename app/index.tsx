db95cdcf755a95f20dc601af54fcf874fcdf9fd0
Anuprita <anupritamhapankar22@gmail.com>
2024-11-15 16:15:24 +0530

    nativewind config added

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View,  StyleSheet } from 'react-native';
+
+export default function Index() {
+  return (
+    <View className="bg-slate-700">
+      <Text className='text-white'>Home screen</Text>
+    </View>
+  );
+}
